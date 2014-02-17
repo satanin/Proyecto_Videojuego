1c9240920f32a23a6842e840306cd82ca3d68c5e
Raul Garcia <satanin@gmail.com>
2014-02-17 22:27:07 +0100

    Modificando el generador, nueva clase Mapa y nuevo Menu para probarla.

---
+++ Proyecto Videojuego/src/Mapa.java
@@ -0,0 +1,88 @@
+
+public class Mapa {
+	private int[][] miMapa;
+	private int minimo = 4;
+	private int maximo = 20;
+	private int quedanx, quedany;
+	private int tamanyoHab = 1;
+	private int posi=0;
+	private int posj=0;
+	private int contador=0;
+	
+	public Mapa(int size) {
+		System.out.println("Intentando generar un mapa cuadrado de tamaño: "+size+"x"+size);
+		quedanx = size-1;
+		quedany = size-1;
+		miMapa = new int[size][size];
+		try{
+			
+			while(quedany>minimo){
+				
+				while(posi<size-1){
+					posj=0;
+					maximo = 20;
+					minimo = 4;
+					quedanx = size-1;
+					
+					while(posj<size-1){
+						if(quedanx<maximo){
+							maximo = quedanx;
+						}
+						if (quedanx-maximo<minimo){
+							minimo = maximo-1;
+						}
+						
+						
+					// genero una habitación con un tamaño aleatorio entre 4 y 20 posiciones;
+						tamanyoHab = minimo + (int)(Math.random() * (maximo-minimo)+1);
+						System.out.println("El tamaño de la habitación es: "+tamanyoHab);
+						
+						// Intento rellenar el array con la nueva habitación
+		//				System.out.println("pos vale: "+posj);
+						for (int i=0;i<=10;i++){
+	//						System.out.println("i vale: "+i);
+							for(int j = 0 ;j<=tamanyoHab;j++){
+								if(i == 0| j == 0 | i == size-1 | j == size-1 | i == 10 | j == tamanyoHab){
+									miMapa[posi+i][posj+j] = 1;
+								}else {
+									miMapa[posi+i][posj+j] = 0;
+								}
+								
+		//						System.out.println("j vale: "+j);
+							}
+							
+							
+							
+	//						System.out.println("pos vale: "+posi);
+														
+							
+	
+						}
+						
+						for (int k=0;k<miMapa.length;k++){
+							for (int l=0;l<miMapa.length;l++){
+								System.out.print(miMapa[k][l]);
+							}System.out.println();
+						}
+					
+						System.out.println("Habitación Rellenada!");
+						quedanx = quedanx - tamanyoHab;
+		//				quedanx = quedanx - tamanyoHab;
+						posj = posj + tamanyoHab;
+					}
+					posi = posi+10;
+					quedany = quedany-10;
+				}
+			}
+		
+		}finally {
+			System.out.println("OOPS!");
+		}
+//		catch (Exception e){
+//			System.out.println("Ha ocurrido algo malo");
+//		}
+			
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- Proyecto Videojuego/src/Principal.java
+++ Proyecto Videojuego/src/Principal.java
@@ -3,22 +3,35 @@ import java.awt.EventQueue;
 
 public class Principal {
 
+//	public static void main(String[] args) {
+//
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					Mapas esteMapa = new Mapas();
+//					String m[][] = esteMapa.devuelveMapa();
+//					JuegoWB frame = new JuegoWB(m);
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	
+//	}
 	public static void main(String[] args) {
-
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Mapas esteMapa = new Mapas();
-					String m[][] = esteMapa.devuelveMapa();
-					JuegoWB frame = new JuegoWB(m);
+					MenuTest frame = new MenuTest();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-	
 	}
+
 	
 
 }
